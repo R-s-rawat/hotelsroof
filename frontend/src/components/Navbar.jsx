17506290293dd6e3e3febe6c67fa06e63cba27ce
R-s-rawat <rohitrawat590590@gmail.com>
2025-05-15 06:39:14 +0530

    imported library, minor change, error

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { IoClose, IoMenuSharp } from "react-icons/io5"
