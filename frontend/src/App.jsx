a809f1692cf68ab5943777b9e6e9035ba17958cb
Rohit <rohitrawat590590@gmail.com>
2025-07-12 10:45:34 +0530

    updated footer with date->year, rights reserved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-    <div className='bg-bgPrimary min-h-screen flex flex-col'>
-      <Navbar/>
-      <div className='flex-grow'>
-        <Outlet/>
+      <div className="bg-bgPrimary min-h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-grow">
+          <Outlet />
+        </div>
+        {/* <footer className='mt-auto'>Footer</footer> */}
+        <footer className="bg-white text-center text-gray-500 text-sm py-4 border-t">
+          &copy; {new Date().getFullYear()} HotelsRoof. All rights reserved.
+        </footer>
       </div>
-      <footer className='mt-auto'>Footer</footer>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
